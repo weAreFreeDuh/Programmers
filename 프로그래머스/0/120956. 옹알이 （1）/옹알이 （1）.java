225a93c0532ebb85ef389a444a500d82ed14cbb2
weAreFreeDuh <121088782+weAreFreeDuh@users.noreply.github.com>
2024-04-25 16:43:52 +0900

    [level 0] Title: 옹알이 (1), Time: 0.21 ms, Memory: 81.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(String[] babbling) {
+        int answer = 0;
+        for(int i = 0; i < babbling.length; i++){
+            babbling[i] = babbling[i].replace("aya", "*").replace("ye", "*").replace("woo", "*").replace("ma", "*");
+            
+            if(babbling[i].replace("*","").equals("")) answer++;
+            
+        }
+        
+        return answer;
+    }
+}
